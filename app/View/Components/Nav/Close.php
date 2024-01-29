c7ff47483ff8981f5eac88c989a176786bc799fa
ahmeddi <ah.diyana@gmail.com>
2024-01-29 05:45:34 +0000

    add new menu

--- app/View/Components/Nav/Close.php
+++ app/View/Components/Nav/Close.php
@@ -21,6 +21,6 @@ class Close extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.nav.close.blade.php');
+        return view('components.Nav.Close');
     }
 }
